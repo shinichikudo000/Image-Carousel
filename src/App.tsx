573ae8246dec2c99077f7f09e319541880443c2a
shinichikudo000 <acostayuukichi@gmail.com>
2023-12-11 15:13:36 +0800

    Day 9

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { content } from './image'
+import './App.css'
+
+function App() {
+  const [imageIndex, setImageIndex] = useState<number>(0)
+  const nextIndex = () => {
+    if(imageIndex < content.length - 1) {
+      setImageIndex((prev: number) => prev + 1)
+    }
+  }
+
+  const prevIndex = () => {
+    if(imageIndex > 0) {
+      setImageIndex((prev: number) => prev - 1)
+    }
+  }
+
+  return (
+    <> 
+       <div className="feature">
+          <img src={`./images/${content[imageIndex].image}`} alt="Featured" />
+          <div className="caption">{content[imageIndex].caption}</div>
+        </div>
+
+        <div className="thumbnails">
+          <ul>
+            {content.map((image, index) => {
+              let style = index === imageIndex ? 'selected' : ''
+              return <li className={style}>
+                        <a href="#">
+                          <img src={`./images/${image.image}`} alt="" />
+                        </a>
+                      </li>
+            })}
+          </ul>
+        </div>
+
+        <a href="#" className="left" onClick={prevIndex}><img src="./images/chevron.svg" alt="" /></a>
+        <a href="#" className="right" onClick={nextIndex}><img src="./images/chevron.svg" alt="" /></a>
+    </>
+  )
+}
+
+export default App
